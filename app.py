710cfa70583de9256343cccb15016aee4d146a2a
Francois rabanel <rabanel.francois@gmail.com>
2018-10-10 20:12:43 +0200

    init commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('pages/index.html')
+
+@app.route('/projets')
+def projets():
+    return render_template('pages/projets.html')
+
+@app.route('/rec21')
+def rec21():
+    return render_template('pages/rec21.html')
+
+@app.route('/radio')
+def radio():
+    return render_template('pages/radio.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('pages/contact.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
